cfbc00e1c3d227773d6897046ae010f9842be748
Colby Birkhead <birkheadc@gmail.com>
2022-09-10 00:35:57 +0900

    Add light/dark theme toggle; Decide light theme

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,25 @@ import Navbar from './components/navbar/Navbar';
 function App() {
 
   function handleChangeTheme():void {
-    console.log("Change theme now.");
+    let theme: string = getCurrentTheme();
+    if (theme === 'light') theme = 'dark';
+    else theme = 'light';
+    localStorage.setItem('birkheadc_homepage-theme', theme);
+    loadTheme(theme);
   }
+
+  function getCurrentTheme(): string {
+    let localTheme: string | null = localStorage.getItem('birkheadc_homepage-theme');
+    if (localTheme) return localTheme;
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  }
+
+  function loadTheme(theme: string) {
+    const root = document.querySelector(':root');
+    if (root) root.setAttribute('color-scheme', theme);
+  }
+
+  loadTheme(getCurrentTheme());
 
   return(
     <div>
