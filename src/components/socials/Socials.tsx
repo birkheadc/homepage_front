7d54966df08b5ae62419b4499d93b51d4ecbaa3b
Colby Birkhead <birkheadc@gmail.com>
2022-09-17 01:33:42 +0900

    Make social icon links open in new tab

--- src/components/socials/Socials.tsx
+++ src/components/socials/Socials.tsx
@@ -6,9 +6,9 @@ import li_icon from '../../assets/images/social-icons/linkedin.png';
 function Socials() {
     return (
         <ul className='socials-ul'>
-            <li><a href='https://www.facebook.com/#!/profile.php?id=100000139877934'><img alt='My Facebook' className='icon-link social-icon' src={fb_icon}></img></a></li>
-            <li><a href='https://github.com/birkheadc'><img alt='My Github' className='icon-link social-icon' src={gh_icon}></img></a></li>
-            <li><a href='https://www.linkedin.com/in/colby-birkhead'><img alt='My LinkedIn' className='icon-link social-icon' src={li_icon}></img></a></li>
+            <li><a href='https://www.facebook.com/#!/profile.php?id=100000139877934' target='_blank'><img alt='My Facebook' className='icon-link social-icon' src={fb_icon}></img></a></li>
+            <li><a href='https://github.com/birkheadc' target='_blank'><img alt='My Github' className='icon-link social-icon' src={gh_icon}></img></a></li>
+            <li><a href='https://www.linkedin.com/in/colby-birkhead' target='_blank'><img alt='My LinkedIn' className='icon-link social-icon' src={li_icon}></img></a></li>
         </ul>
     );
 }
